aadf3770e1d2846e069ea234b7cba97aa95e6ccc
elchocolateropaco12-blip <elchocolateropaco12@gmail.com>
2026-08-09 13:55:55 +0200

    Create RegistrarSW.tsx

---
+++ components/RegistrarSW.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function RegistrarSW() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((reg) => console.log('SW registrado:', reg.scope))
+        .catch((err) => console.error('Error al registrar SW:', err));
+    }
+  }, []);
+
+  return null;
+}
